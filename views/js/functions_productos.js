bbffe4f0124879d02f906361f3ba2c70461fee05
Julianore2004 <julianore79@gmail.com>
2024-10-17 11:58:17 -0500

    a

--- views/js/functions_productos.js
+++ views/js/functions_productos.js
@@ -1,1 +1,13 @@
-console.log("hola mundo");
+async function registrar_producto() {
+
+    let codigo = document.getElementById('codigo').value;
+    let nombre = document.querySelector('#nombre').value;
+    console.log(codigo);
+    console.log(nombre);
+    let detalle = document.querySelector('#detalle').value;
+    let precio = document.querySelector('#precio').value;
+    let stock = "stock";
+    let categoria = "categoria";
+    let imagen = "imagen";
+    let proveedor = "proveedor";
+}
--- views/nuevo-producto.php
+++ views/nuevo-producto.php
@@ -1,42 +1,41 @@
 <div>
-    <p>
-        FORMULARIO PARA INSERTAR PRODUCTO</p>
+    <p>FORMULARIO PARA INSERTAR PRODUCTO</p>
 </div>
 <form action="">
 
     <div>
         <label for="codigo">Código:</label>
-        <input type="text" name="codigo" id="codigo" class="form-control" required>
+        <input type="text" name="codigo" id="codigo" class="form-control" >
     </div>
     <div>
         <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" id="nombre" class="form-control" required>
+        <input type="text" name="nombre" id="nombre" class="form-control" >
     </div>
     <div>
         <label for="detalle">Detalle:</label>
-        <input type="text" name="detalle" id="detalle" class="form-control" required>
+        <input type="text" name="detalle" id="detalle" class="form-control" >
     </div>
     <div>
         <label for="precio">Precio:</label>
-        <input type="number" name="precio" id="precio" class="form-control" required>
+        <input type="number" name="precio" id="precio" class="form-control" >
     </div>
     <div>
         <label for="stock">Stock:</label>
-        <input type="number" name="stock" id="stock" class="form-control" required>
+        <input type="number" name="stock" id="stock" class="form-control" >
     </div>
     <div>
         <label for="categoria">Categoría:</label>
-        <input type="number" name="categoria" id="categoria" class="form-control" required>
+        <input type="number" name="categoria" id="categoria" class="form-control" >
     </div>
     <div>
         <label for="img">Imagen del Producto:</label>
-        <input type="text" name="img" id="img" class="form-control" required>
+        <input type="text" name="img" id="img" class="form-control" >
     </div>
     <div>
         <label for="proveedor">Proveedor:</label>
-        <input type="number" name="proveedor" id="proveedor" class="form-control" required>
+        <input type="number" name="proveedor" id="proveedor" class="form-control" >
     </div>
-<button type="submit" class="">Registrar</button>
+<button type="button" class="btn btn-success" onclick="registrar_producto();">Registrar</button>
 </form>
 
 <script src="<?php echo BASE_URL?>views/js/functions_productos.js">
