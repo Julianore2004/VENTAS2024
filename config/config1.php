51334ccd1e7911ccb1322d43c1e3de0b47efcca3
Julianore2004 <julianore79@gmail.com>
2024-11-09 22:52:32 -0500

    Update config1.php

--- config/config1.php
+++ config/config1.php
@@ -4,7 +4,7 @@
 const BD_HOST = 'localhost';
 const BD_NAME = 'sistema_venta';
 const BD_USER = 'root';
-const BD_PASSWORD = 'guardian.tale3';
+const BD_PASSWORD = '';
 
 const BD_CHARSET = 'utf8';
 
