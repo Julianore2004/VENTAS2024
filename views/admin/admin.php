be56b61a76d518afd615a87fe911f2b58a2f4ba4
Julianore2004 <julianore79@gmail.com>
2024-12-12 08:19:39 -0500

    aa

--- views/admin/admin.php
+++ views/admin/admin.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel Administrativo</title>
-    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
@@ -19,7 +18,7 @@
             background: #f0f2f5;
             margin: 0;
             display: flex;
-            height: 100vh;
+            height: auto;
             color: #333;
         }
 
